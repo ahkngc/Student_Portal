4ae2033b1c56140e85c90ef2484af809a1961d9a
Youssefashraf644 <youssef.elsayed1978@gmail.com>
2025-12-09 23:56:23 +0200

    Add Student class to app.py
    Created Student class with constructor for student details

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import os
 
 app = Flask(__name__)
 app.secret_key = "secret_key"
+class Student:
+    def __init__(self, student_id, email, password, name, age, class_, phone):
+        self.id = student_id
+        self.email = email
+        self.password = password
+        self.name = name
+        self.age = age
+        self.class_ = class_
+        self.phone = phone
