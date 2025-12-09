852b64d6ed3c9f47dade33276ec2d5c11277a0d5
Youssef Ashraf <youssef.elsayed1978@gmail.com>
2025-12-10 01:14:56 +0200

    Implement StudentManager for loading students
    Added StudentManager class to manage student data loading from a file.

--- app.py
+++ app.py
@@ -12,3 +12,27 @@ class Student:
         self.age = age
         self.class_ = class_
         self.phone = phone
+
+
+
+
+
+
+
+#issue 7
+class StudentManager:
+    def __init__(self, filename="students.txt"):
+        self.filename = filename
+        self.students = self.load_students()
+
+    def load_students(self):
+        students = []
+        if os.path.exists(self.filename):
+            with open(self.filename, "r") as f:
+                for line in f:
+                    line = line.strip()
+                    if line:
+                        sid, email, pwd, name, age, cls, phone = line.split(",")
+                        students.append(Student(sid, email, pwd, name, age, cls, phone))
+        return students        
+        
