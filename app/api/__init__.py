8276f240da7e7375179c993bc78d6df5799c0804
ahmed sanad <aonsshow123@gmail.com>
2025-12-20 21:44:54 +0200

    chore: remove unused legacy api module

--- app/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-api_bp = Blueprint("api", __name__)
-
-from . import routes
-from . import students  
-from . import courses
-from . import enrollments
-from . import grades  
